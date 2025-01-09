6ad6b83bda8eb0f9921e3e3d2bc3b029bac1a994
Marion Joseph Agustin <131208352+martforge@users.noreply.github.com>
2025-01-09 14:18:43 +0800

    update

--- pygame-studies.py
+++ pygame-studies.py
@@ -6,6 +6,8 @@ screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
 test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
+game_active = True
+
 
 # Image BACKGROUND
 sky_surface = pygame.image.load('graphics/Sky.png').convert()
@@ -30,43 +32,47 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if player_rect.collidepoint(event.pos): 
-                player_gravity = -20
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                player_gravity = -20
+
+        if game_active:
+            if event.type == pygame.MOUSEBUTTONDOWN:  
+                if player_rect.collidepoint(event.pos) and player_rect.bottom >= 300:             
+                    player_gravity = -20
+                
+            if event.type == pygame.KEYDOWN:  
+                if event.key == pygame.K_SPACE and player_rect.bottom >= 300:
+                    player_gravity = -20
+        
+        else:
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                game_active = True
+                snail_rect.left = 800
 
 
-        if event.type == pygame.KEYUP:
-            print('key up')
+    if game_active:
+        # Drawing background and surfaces
+        screen.blit(sky_surface, (0, 0))
+        screen.blit(ground_surface, (0, 300))
+        # Draw filled rectangle and border
+        pygame.draw.rect(screen, '#c0e8ec', score_rect)  # Filled rectangle
+        pygame.draw.rect(screen,'#c0e8ec',score_rect.inflate(10,10))  # Border
+        screen.blit(score_surf, score_rect)
 
+        snail_rect.x -= 4
+        if snail_rect.right <= 0: snail_rect.left = 800
+        screen.blit(snail_surf, snail_rect)
 
+        # Player
+        player_gravity += 1
+        player_rect.y += player_gravity
+        if player_rect.bottom >= 300: player_rect.bottom = 300
 
+        screen.blit(player_surf, player_rect)
 
-    # Drawing background and surfaces
-    screen.blit(sky_surface, (0, 0))
-    screen.blit(ground_surface, (0, 300))
-
-    # Draw filled rectangle and border
-    pygame.draw.rect(screen, '#c0e8ec', score_rect)  # Filled rectangle
-    pygame.draw.rect(screen,'#c0e8ec',score_rect.inflate(10,10))  # Border
-
-
-    # pygame.draw.ellipse(screen, 'Brown', pygame.Rect(50, 200, 100, 100))
-
-    screen.blit(score_surf, score_rect)
-
-    snail_rect.x -= 4
-    if snail_rect.right <= 0: snail_rect.left = 800
-    screen.blit(snail_surf, snail_rect)
-
-    # Player
-    player_gravity += 1
-    player_rect.y += player_gravity
-    if player_rect.bottom >= 300: player_rect.bottom = 300
-
-    screen.blit(player_surf, player_rect)
+        # collision
+        if snail_rect.colliderect(player_rect):
+            game_active = False
+    else:
+        screen.fill('Yellow')
 
     pygame.display.update()
     clock.tick(60)
